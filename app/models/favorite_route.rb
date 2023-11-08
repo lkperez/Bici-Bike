6d5a1a6c662395b1c31b4a126e079373f897abe2
Kika <kikakovaleski@gmail.com>
2023-11-08 16:33:38 -0500

    created FavoriteRoute and FavoriteStation models

---
+++ app/models/favorite_route.rb
@@ -0,0 +1,2 @@
+class FavoriteRoute < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_08_212343) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_08_213205) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorite_routes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorite_stations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
