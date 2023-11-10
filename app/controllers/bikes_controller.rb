af491ced043fba1ca70095d139404143527c3cfd
Kika <kikakovaleski@gmail.com>
2023-11-09 20:26:48 -0500

    added controllers

---
+++ app/controllers/bikes_controller.rb
@@ -0,0 +1,13 @@
+class BikesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'routes/index'
+  get 'routes/show'
+  get 'routes/new'
+  get 'routes/edit'
+  get 'rides/index'
+  get 'rides/show'
+  get 'rides/new'
+  get 'rides/edit'
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/edit'
+  get 'reports/index'
+  get 'reports/show'
+  get 'reports/new'
+  get 'reports/edit'
+  get 'members/index'
+  get 'members/show'
+  get 'members/new'
+  get 'members/edit'
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'bikes/new'
+  get 'bikes/edit'
   root to: "stations#index"
 
   resources :stations
