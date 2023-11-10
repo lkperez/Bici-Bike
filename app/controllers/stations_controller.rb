72eb08be9b96a4abd084a78c3cd1615e544f201e
Kika <kikakovaleski@gmail.com>
2023-11-09 19:57:50 -0500

    routed individual show pages

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,7 +1,11 @@
 class StationsController < ApplicationController
-  
+
   def index
     @stations = Station.all.order(identifier: :asc)
   end
-  
+
+  def show
+    @stations = Station.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "stations#index"
-  
+
+  resources :stations
+
 end
