1f33278f33b12119966c4e12869c672b0f523ee1
Jillian Lehosky <jillianlehosky@gmail.com>
2023-11-12 18:37:01 -0500

    basic navbar added with links to about and pricing working

---
+++ app/views/stations/station.rb
@@ -0,0 +1,11 @@
+class Station < ApplicationRecord
+  validates_presence_of    :identifier,
+                           :name,
+                           :address
+  validates_uniqueness_of  :identifier
+
+  has_many :docked_bikes, class_name: :Bike, foreign_key: :current_station_id
+  has_many :favorite_stations
+  belongs_to :map
+
+end
