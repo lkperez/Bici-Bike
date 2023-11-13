ec15c5f9bf4206da86b92f364e1a2e9da9d507ff
Laisha <lkoonperez@smith.edu>
2023-11-13 16:02:53 -0500

    modified:   app/controllers/sessions_controller.rb 	modified:   app/views/layouts/_messages.html.erb 	modified:   app/views/layouts/application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,15 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
           log_in user
-          #redirect_to root_path, notice: "Login was successful!"
-          render turbo_stream: turbo_stream.visit(root_path)
+          redirect_to root_path, notice: "Login was successful!"
         else 
            flash.now[:alert] = 'Invalid username/password'
-           render 'new'
+           render :new
         end
     end
 
     def destroy
-      #log_out
-      #redirect_to root_path, notice: 'Logged out'
-      log_out if logged_in?
-      render turbo_stream: turbo_stream.visit(root_path)
+      log_out
+      redirect_to root_path, notice: 'Logged out'
     end
 end
