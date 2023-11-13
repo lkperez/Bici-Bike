dde346679d30ecd05be5d2783680a9cc8a470d0d
Laisha <lkoonperez@smith.edu>
2023-11-13 15:09:38 -0500

    modified:   app/controllers/sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:session][:email])
-        if user && user.authenticate(params[:session][:password])
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
           log_in user
           redirect_to root_path, notice: "Login was successful!"
         else 
