8784b78270579b227dbe64cbeaedccd649dc92ff
Kika <kikakovaleski@gmail.com>
2023-11-13 14:49:09 -0500

    slightly improves form

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,13 +1,19 @@
 class RidesController < ApplicationController
   def index
+    @rides = Ride.all.order(id: :asc)
   end
 
   def show
+    @ride = Ride.find(params[:id])
   end
 
   def new
-    @count = Ride.count
-    @ride = Ride.new(id: @count + 1)
+    @ride = Ride.new(params[:ride])
+    if @ride.save
+      puts "no error"
+    else
+      puts "error"
+    end
   end
 
   def edit
