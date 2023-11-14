948711743f98e653734af0da5a6b5ef87bb05f2d
Kika <kikakovaleski@gmail.com>
2023-11-14 11:50:59 -0500

    removes bike from current station when ride starts

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -15,6 +15,8 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.new(params.require(:ride).permit(:member_id, :bike_id, :length, :id))
     if @ride.save
+      @ride.bike.current_station_id = nil
+      @ride.bike.save
       redirect_to rides_path
     else
       redirect_to new_ride_path
