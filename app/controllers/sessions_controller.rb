ca3bef8e87ddacd54f5cc200b1682c5fe80b8566
Laisha <lkoonperez@smith.edu>
2023-11-17 17:08:58 -0500

    modified:   app/assets/stylesheets/application.css 	modified:   app/controllers/sessions_controller.rb 	modified:   app/models/user.rb 	modified:   app/views/.DS_Store 	new file:   app/views/sessions/.DS_Store 	modified:   app/views/sessions/new.html.erb 	modified:   app/views/stations/index.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
-    def new
-      @user = User.new 
-    end
+  def new
+    @user = User.new 
+  end
 
-    def create
-      @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-          log_in(@user) 
-          redirect_to root_path, notice: 'Login was successful!'
-        else 
-           flash.now[:alert] = 'Invalid username/password'
-           render 'new'
-        end
-    end
+  def create
+    @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+        log_in(@user) 
+        redirect_to root_path, notice: 'Login was successful!'
+      else 
+         flash.now[:alert] = 'Invalid username/password'
+         render 'new'
+      end
+  end
 
-    def destroy
-      log_out()
-      redirect_to root_path, notice: 'Logged out'
-    end
+  def destroy
+    log_out()
+    redirect_to root_path, notice: 'Logged out'
+  end
 end
