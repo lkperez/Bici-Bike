40d6258a11ba5fad7dcbf496b06736f9603818ac
Laisha <lkoonperez@smith.edu>
2023-11-18 13:42:48 -0500

    modified:   app/views/sessions/new.html.erb 	modified:   app/views/users/new.html.erb 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+  resources :users, only: [:new, :create]
 end
