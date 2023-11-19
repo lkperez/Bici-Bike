8abf6797ea39c8cac49165d50e384bab5df99374
Laisha <lkoonperez@smith.edu>
2023-11-19 18:31:21 -0500

    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,11 @@ Rails.application.routes.draw do
   
   
   
-  
+  root to: 'stations#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get '/signup', to: 'users#new', as: 'signup'
   post '/signup', to: 'users#create'
   resources :users, only: [:new, :create]
+end
