402239fd84dd295ac7d307f14321abd11b6fb1e0
Kika <kikakovaleski@gmail.com>
2023-12-03 19:39:29 -0500

    added completed boolean column to the rides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_213103) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_003611) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_213103) do
     t.datetime "updated_at", null: false
     t.integer "member_id"
     t.integer "bike_id"
+    t.boolean "returned"
   end
 
   create_table "routes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
