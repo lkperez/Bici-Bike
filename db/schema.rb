e2d9d861f95e665068dce1166d11cce75fd6c80e
Kika <kikakovaleski@gmail.com>
2023-12-03 20:47:52 -0500

    makes times more legible

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_003611) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_013949) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -74,7 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_003611) do
   create_table "rides", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "length"
     t.datetime "timeStart"
-    t.string "timeEnd"
+    t.datetime "timeEnd", precision: nil
     t.string "datetime"
     t.integer "rideCost"
     t.datetime "created_at", null: false
