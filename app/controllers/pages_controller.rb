1c03a0db3f80527dcd59bea50de1666db2bd96f2
tolentinoo <98605856+tolentinoo@users.noreply.github.com>
2023-12-05 13:49:46 -0500

    added route and action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,7 @@ class PagesController < ApplicationController
     def show
         render template: "pages/#{params[:page]}"
     end
+    def map
+        # Your 'map' action code goes here
+    end
   end 
