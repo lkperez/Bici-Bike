817f3c355b9018b2fa9653820a1569b68265ea66
Kika <kikakovaleski@gmail.com>
2023-12-11 00:09:18 -0500

    adds review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,19 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @review = Review.new()
+  end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:member_id, :route_id, :starRating, :textReview))
+    if @review.save
+      redirect_to route_path(@review.route_id)
+    else
+      render('new')
+    end
+  end
+
+  def update
   end
 
   def edit
