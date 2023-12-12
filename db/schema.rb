c21b5c37bb05ccbc153968e73ed41c9acb32edf1
Kika <kikakovaleski@gmail.com>
2023-12-12 00:49:58 -0500

    fix pricing nav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_12_11_032937) do
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
@@ -114,13 +113,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_032937) do
     t.datetime "updated_at", null: false
     t.integer "map_id"
     t.integer "total_docks"
-  end
-
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
